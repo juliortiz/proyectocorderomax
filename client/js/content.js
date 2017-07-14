084b62fc45f889f99046a79cb97feb058a31c222
juliortiz <jcortizojeda@gmail.com>
2017-07-14 13:28:35 -0500

    header vista mobile terminado

--- client/js/content.js
+++ client/js/content.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	console.log("Hi there! :)")
+	console.log("Hi there! sssss:)")
 });
+
+$(document).ready(function() {
+	console.log("JEJE XD");
+
+	$('.flexslider').flexslider({
+		animation: "slide"
+	});
+
+});
+
+
